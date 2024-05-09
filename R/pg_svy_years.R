3f38478aadfe0aa134ece1e13b431fbd82115828
R.Andres Castaneda <acastastanedaa@worldbank.org>
2024-05-09 10:42:54 -0400

    update

--- R/pg_svy_years.R
+++ R/pg_svy_years.R
@@ -13,6 +13,7 @@ version  <- "20240429_2017_01_02_INT"
 ppp_year <- sub("(.+_)([0-9]{4})(_.+)", "\\2", version) |>
   as.numeric()
 
+## ppp year --------
 if (ppp_year == 2017) {
   ps <- 25
 } else if (ppp_year == 2011) {
@@ -21,6 +22,9 @@ if (ppp_year == 2017) {
   ps <- 25
 }
 
+
+
+# get data -----------
 data_files <-
   Sys.getenv("PIPAPI_DATA_ROOT_FOLDER_LOCAL") |>
   fs::path(version, "survey_data") |>
@@ -38,7 +42,7 @@ to_drop <- which(grepl("GROUP$", files_names))
 files_names <- files_names[-to_drop]
 data_files  <- data_files[-to_drop]
 
-
+## load data ---------
 tictoc::tic()
 ld <- purrr::imap(data_files, \(x, y) {
   fst::read_fst(x, as.data.table = TRUE) |>
@@ -49,6 +53,9 @@ dt <- rowbind(ld)
 tictoc::toc()
 
 
+# calculate PG ---------
+
+## by area ---------
 pg_area <-
   dt |>
   ftransform(pg = ps/welfare) |>
@@ -57,6 +64,7 @@ pg_area <-
   fmean(weight, stub = FALSE) |>
   fungroup()
 
+## national ---------
 pg_national <-
   pg_area |>
   ftransform(area = "national") |>
@@ -65,5 +73,30 @@ pg_national <-
   fmean(weight, stub = FALSE) |>
   fungroup()
 
-rowbind(pg_area, pg_national)
+## Append both ---------
+ft <- rowbind(pg_area, pg_national)
+
+ft[,
+   c("country_code", "year") := tstrsplit(id, split = "_", keep = c(1, 2))
+   ][, id := NULL]
+
+setcolorder(ft, c("country_code", "year"))
+setorderv(ft, c("country_code", "area", "year"))
+
+# Save results-----------
+
+## output dir -----------
+
+outdir <-
+  "p:/03.pip/estimates/temp-Prosperity_Gap/" |>
+  fs::dir_create(version, "svy_years")
+
+## save in different formats ---
+filename <- "PG_svy_year"
+
+fs::path(outdir, filename, ext = "fst") |>
+  fst::write_fst(ft, path = _, compress = 0)
+
+fs::path(outdir, filename, ext = "dta") |>
+  haven::write_dta(ft, path = _)
 
